9aa70cdae10e4b97a3285d06cf4fb0fa3b9b9add
Restyled.io <commits@restyled.io>
2022-07-24 18:38:04 +0000

    Restyled by prettier

--- jest.config.js
+++ jest.config.js
@@ -1,22 +1,22 @@
 module.exports = {
   ...require("./test/jest-common"),
-  collectCoverageFrom : [
+  collectCoverageFrom: [
     "./(components|etc|lib|pages)/**/*.(ts|tsx|js|jsx)",
     "!./(components|etc|lib|pages)/**/__tests__/**/*.test.(ts|tsx|js|jsx)",
     "!./(components|etc|lib|pages)/**/__mocks__/**/*.(ts|tsx|js|jsx)",
     "!./etc/fetchers/fetch-wbw.ts",
     "!./etc/mirror-box.ts",
   ],
-  coverageThreshold : {
-    global : {
-      statements : 84,
-      branches : 76,
-      functions : 82,
-      lines : 84,
+  coverageThreshold: {
+    global: {
+      statements: 84,
+      branches: 76,
+      functions: 82,
+      lines: 84,
     },
   },
-  transform : {
-    "^.+\\.(ts|tsx|js|jsx)$" : "babel-jest",
+  transform: {
+    "^.+\\.(ts|tsx|js|jsx)$": "babel-jest",
   },
-  projects : [ "./test/jest.client.js", "./test/jest.server.js" ],
+  projects: ["./test/jest.client.js", "./test/jest.server.js"],
 };
