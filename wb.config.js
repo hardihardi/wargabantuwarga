0be4e1da1b1c773a622e361e37a8e0f591644679
Restyled.io <commits@restyled.io>
2022-07-24 17:43:44 +0000

    Restyled by prettier

--- wb.config.js
+++ wb.config.js
@@ -1,46 +1,46 @@
 // Read more about next-pwa configuation
 // https://github.com/shadowwalker/next-pwa
 module.exports = {
-  disable : process.env.NODE_ENV === "development",
-  dest : "public",
-  register : true,
-  scope : "/",
-  sw : "service-worker.js",
-  publicExcludes : [
+  disable: process.env.NODE_ENV === "development",
+  dest: "public",
+  register: true,
+  scope: "/",
+  sw: "service-worker.js",
+  publicExcludes: [
     "/robots.txt",
     "/sitemap.xml",
     "/sitemap-*.xml",
     "/google6feea491511f41d3.html",
     "/admin/**/*",
   ],
-  runtimeCaching : [
+  runtimeCaching: [
     {
-      urlPattern : /^https:\/\/res\.cloudinary\.com\/.*\/image\/upload\//,
-      handler : "CacheFirst",
-      options : {
-        cacheName : "wbw-images",
-        expiration : {
-          maxAgeSeconds : 60 * 60 * 24 * 30,
-          maxEntries : 200,
+      urlPattern: /^https:\/\/res\.cloudinary\.com\/.*\/image\/upload\//,
+      handler: "CacheFirst",
+      options: {
+        cacheName: "wbw-images",
+        expiration: {
+          maxAgeSeconds: 60 * 60 * 24 * 30,
+          maxEntries: 200,
         },
       },
     },
     {
-      urlPattern : /.(png|jpg|jpeg|webp|svg|woff|woff2)$/,
-      handler : "CacheFirst",
-      options : {
-        cacheName : "wbw-assets",
-        expiration : {
-          maxAgeSeconds : 60 * 60 * 24 * 7,
-          maxEntries : 50,
+      urlPattern: /.(png|jpg|jpeg|webp|svg|woff|woff2)$/,
+      handler: "CacheFirst",
+      options: {
+        cacheName: "wbw-assets",
+        expiration: {
+          maxAgeSeconds: 60 * 60 * 24 * 7,
+          maxEntries: 50,
         },
       },
     },
     {
-      urlPattern : /.(js|css)$/,
-      handler : "StaleWhileRevalidate",
-      options : {
-        cacheName : "wbw-jscss",
+      urlPattern: /.(js|css)$/,
+      handler: "StaleWhileRevalidate",
+      options: {
+        cacheName: "wbw-jscss",
       },
     },
   ],
